d4826b558f103315d2cb424f52c164e5435d2b29
kimsejune <tpwns1088@gmail.com>
2017-09-22 16:57:24 +0900

    token을 통한 로그인  error 처리

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const path = require('path')
 const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser')
-
+const expressJwt = require('express-jwt')
 const query = require('./query')
 
 const app = express()
@@ -15,6 +15,17 @@ app.set('view engine', 'pug')
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(bodyParser.json())
 
+const authMiddleware = expressJwt({secret:process.env.SESSION_SECRET})
+
+app.get('/user',authMiddleware, (req, res) => {
+  // authMiddleware가 express-jwt라서 서명할 때의 정보를 가져올 수 있다.
+  query.findUserId(req.user.id)
+    .then(user => {
+      res.send({
+        username: user.username
+      })
+    })
+})
 
 app.get('/', (req,res) => {
   res.render('index.pug')
@@ -40,6 +51,15 @@ app.post('/login', (req, res) => {
         token
       })
     })
+})
+
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send({
+      error: err.name,
+      message: err.message
+    })
+  }
 })
 
 app.listen(process.env.PORT, () => {
--- src/query.js
+++ src/query.js
@@ -31,6 +31,12 @@ module.exports = {
           throw new Error('사용자 이름 혹은 비밀번호가 일치하지 않습니다.')
         })
       })
+  },
+
+  findUserId(id) {
+    return knex('user')
+      .where({id})
+      .first()
   }
 
 }
